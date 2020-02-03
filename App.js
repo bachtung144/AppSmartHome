73d6c1965981b115ab72cfec7a796b7a0be9863b
bacht <bachgtung144007@gmail.com>
2020-02-03 11:00:15 +0700

    add logo

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createSwitchNavigator} from 'react-navigation';
 import Login from './src/Components/Login';
 import ForgetPass from './src/Components/ForgetPass';
 import InputMXN from './src/Components/InputMXN';
 import NewPass from './src/Components/NewPass';
 import SignUp from './src/Components/SignUp';
-
-
+import Splash from './src/Components/Splash';
+import {Dimensions } from "react-native";
+const screenWidth = Math.round(Dimensions.get('window').width);
 const StackNavigator = createStackNavigator({
     LoginScreen:{
         screen: Login,
-        navigationOptions:{
-            title:"Đăng nhập"
+        navigationOptions :{
+            title:"Đăng nhập",
+            // headerTitleContainerStyle :{justifyContent: 'center',alignItems:'center'
+            //     ,color:'red',marginLeft:0,paddingLeft:0,width:"100%"}
+            headerTitleContainerStyle: {
+                alignItems: "center",
+                justifyContent: "center",
+                width : screenWidth - 35
+            },
         }
     },
     ForgetPassScreen:{
@@ -40,11 +49,19 @@ const StackNavigator = createStackNavigator({
         }
     },
 })
-
 const AppContainer = createAppContainer(StackNavigator);
 
+const InitialNavigator = createSwitchNavigator({
+    SplashScreen: {
+        screen : Splash
+    },
+    App: StackNavigator
+});
+const AppContainer_splash =  createAppContainer(InitialNavigator);
 export default class App extends React.Component {
     render() {
-        return <AppContainer />;
+        return (
+            <AppContainer_splash></AppContainer_splash>
+        )
     }
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
 import {Button, View, Text,TextInput } from 'react-native';
+import BackGround from './BackGround';
 
 export default class Login extends Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
             <View>
-                <Text>Quốc gia</Text>
-                <TextInput placeholder={"Số điện thoại"}></TextInput>
-                <TextInput placeholder={"Mật khẩu"}></TextInput>
-                <Button title={"Đăng nhập"} ></Button>
-                <Button title={"Đăng kí tài khoản"} onPress={() => navigate('SignUpScreen')}></Button>
-                <Button title={"Quên mật khẩu"}  onPress={() => navigate('ForgetPassScreen')}>
-
-                </Button>
+                <View>
+                    <BackGround/>
+                </View>
             </View>
         );
     }
+}
+
+const styles ={
+
 }
