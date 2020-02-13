dd9231046df80bb5a33e160ac7928d51ddd239e1
bacht <bachgtung144007@gmail.com>
2020-02-13 09:07:56 +0700

    socket

--- src/Containers/Auth/Home.js
+++ src/Containers/Auth/Home.js
@@ -1,8 +1,7 @@
 
 import React, { Component } from 'react'
-import { Text, View , Button} from 'react-native'
+import { Text, View , Button,TextInput} from 'react-native'
 import io from 'socket.io-client'
-window.navigator.userAgent = 'react-native';
 
 
 
@@ -10,30 +9,26 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: null,
-    }
-    this.sendReqest = this.sendReqest.bind(this);
+      chatMessage: "",
+      chatMessages: []
+    };
+    this.socket = io("http://192.168.99.199:1123");
   }
+  // componentDidMount() {
+  //   this.socket = io("http://192.168.99.199:1123");
+  // }
 
-  componentDidMount() {
-    this.socket = io('http://192.168.99.199:1123');
-  }
-
-  sendReqest = () => {
-    this.socket.on('listDevice', smg=>{
-      this.setState({
-        result: smg,
-      })
-      console.log(smg)
-    })
+   submitChatMessage() {
+     this.socket.emit('listRoom');
+     this.socket.on('listRoom',(response) => console.warn(JSON.parse(response).data))
   }
 
   render() {
+
     return (
-        <View>
-          <View>{this.state.result}</View>
-          <Button title = {'send'} onPress ={this.sendReqest}/>
+        <View >
+          <Button title={'test'} onPress={() => this.submitChatMessage()}/>
         </View>
-    )
+             );
   }
 }
